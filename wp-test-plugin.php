324763de8d4c7893640900de8175aea737408802
unknown <roman.bruskevtsev@gtm-plus.com>
2022-05-18 14:10:45 +0300

    update plugin

--- wp-test-plugin.php
+++ wp-test-plugin.php
@@ -11,7 +11,7 @@
 * Plugin Name:       Test Plugin
 * Plugin URI:        https://github.com/romanbruskevtsev/wp-test-plugin
 * Description:       Simple WP Test Plugin
-* Version:           1.0.4
+* Version:           1.0.5
 * Requires at least: 5.2
 * Requires PHP:      7.3
 * Author:            Roman Bruskevtsev
